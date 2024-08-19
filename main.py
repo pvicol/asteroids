780e6e7511f824fade8f4d2a1777ca09c8d2e96a
pvicol <vicolp1@gmail.com>
2024-08-19 13:16:15 -0700

    Player movement

--- main.py
+++ main.py
@@ -3,17 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+
         screen.fill(color='black')
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
