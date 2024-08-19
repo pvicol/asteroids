328a49d8bcca7f708f6d7d2c45291d79c613c1e9
pvicol <vicolp1@gmail.com>
2024-08-19 13:20:32 -0700

    Added group control

--- main.py
+++ main.py
@@ -8,18 +8,26 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame_clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        for item in updatable:
+            item.update(dt)
 
         screen.fill(color='black')
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = pygame_clock.tick(60) / 1000
 
